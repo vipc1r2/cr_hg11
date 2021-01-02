695f15a6b0cf3a3778c48de7a8c2cbc44b5c4e13
damaoermaohchr <vip_c1r2@163.com>
2021-01-02 17:33:59 +0800

    添加demo，ignore文件

---
+++ cr_study_hg11/demo.py
@@ -0,0 +1,14 @@
+print(2 + 2)
+print('C:\\some\\name')
+print(r'C:\some\name')   # 前边加r，不用输入两个\
+
+print('Py' 'thon')  # 相当于两个字符串连在一起
+
+x='abc'
+print(f'C:\\some\\name\\{x}')
+
+print("C:\\some\\name\\{y}\\{x}".format(y=x, x=123))
+
+
+squares = [1, 4, 9, 16, 25]
+print(squares)
